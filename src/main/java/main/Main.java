93df7a6b3011b15ae40832aaa2cff23b8fcf95d0
Elmir Shamsutdinov <shamsut.elm@gmail.com>
2024-07-14 20:01:56 +0300

    improved logic
    updated pattern RULE_NAME
    changed list to map

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,7 +2,9 @@ package main;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,10 +13,12 @@ import reader.G4Reader;
 
 public class Main {
 
-    private static final Pattern RULE_NAME = Pattern.compile("[_|a-z]*\n\s\s\s\s:");
+    private static final Pattern RULE_NAME = Pattern.compile("\n\s?[_|a-z]*\n\s\s\s\s:");
+    private static final Pattern CHILD_PATTERN = Pattern.compile("[a-z][_a-z]*");
+
     private static final String PATH = "D:\\Work\\IT\\java\\eclipse_rcp\\pgcodekeeper"
             + "\\ru.taximaxim.codekeeper.core\\antlr-src\\SQLParser.g4";
-    private static List<Rule> rules = new ArrayList<>();
+    private static Map<String, Rule> rules = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         var parserRules = G4Reader.readParser(PATH);
@@ -26,31 +30,56 @@ public class Main {
                 start = m.group();
             } else if (end == null) {
                 end = m.group();
-                rules.add(getRule(start, end, parserRules));
+                getRule(start, end, parserRules);
             } else {
                 start = end;
                 end = m.group();
-                rules.add(getRule(start, end, parserRules));
+                getRule(start, end, parserRules);
             }
         }
         if (end != null) {
             // collect the last rule
-            rules.add(getRule(end, end, parserRules));
+            getRule(end, end, parserRules);
         }
 
 //        for (var r : rules) {
 //            if (r.getName().equals("operator_args_parser")) System.out.println(true);
 //        }
 //        System.out.println(rules.get(0));
-//        System.out.println(rules.size());
-        for (var r : rules) {
-            System.out.println("-------------------------------------------------");
-            System.out.println(r);
-        }
+        System.out.println(rules.size());
+
+//        for (var r : rules.values()) {
+//            System.out.println("-------------------------------------------------");
+//            System.out.println(r);
+//        }
+
+//        for (var s : rules.entrySet()) {
+//            var parent = s.getValue();
+//            m = CHILD_PATTERN.matcher(parent.getBody());
+//            if (parent.getName().equals("indirection")) {
+//                System.out.println("hi");
+//            }
+//            while (m.find()) {
+//                var childName = m.group();
+//                var child = rules.get(childName);
+//                if (child == null) {
+//                    System.out.println(parent.getName());
+//                    System.out.println(childName);
+//                }
+//                parent.addChild(child);
+//            }
+//        }
+
+//        for (var r : rules.values()) {
+//            System.out.println("----------------------------------------------------");
+//            System.out.println(r.getName());
+//            System.out.println(r.getChildren().size());
+//            System.out.println(r.getParents().size());
+//        }
     }
 
-    private static Rule getRule(String start, String end, String sb) {
-        var name = start.substring(0, start.indexOf('\n'));
+    private static void getRule(String start, String end, String sb) {
+        var name = start.substring(start.indexOf('\n'), start.length() - 1);
         var rule = new Rule(name);
         String body;
         int startIndex = sb.indexOf(start) + start.length();
@@ -58,20 +87,12 @@ public class Main {
             // use this logic if this last rule
             body = sb.substring(startIndex);
             rule.setBody(body);
-            return rule;
+            rules.put(name, rule);
+            return;
         }
         int endIndex = sb.indexOf(end);
-        // TODO find the way how remove this checks. maybe change pattern in RULE_NAME can fix it.
-        if (startIndex > endIndex) {
-            // start index can be larger than end index some other parse rule ends by argument end
-            endIndex = sb.indexOf('\n' + end);
-            if (endIndex < 0) {
-                // end index can be less than 0 when before parser rule have indent
-                endIndex = sb.indexOf("\n " + end);
-            }
-        }
         body = sb.substring(startIndex, endIndex);
         rule.setBody(body);
-        return rule;
+        rules.put(name, rule);
     }
 }
