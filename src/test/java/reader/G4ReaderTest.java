e076363393076c4798431c621edeefbc356ff9a0
Elmir Shamsutdinov <shamsut.elm@gmail.com>
2025-01-05 21:27:55 +0300

    added test

---
+++ src/test/java/reader/G4ReaderTest.java
@@ -0,0 +1,20 @@
+package reader;
+
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.File;
+
+import org.junit.jupiter.api.Test;
+
+public class G4ReaderTest {
+
+    private final static String PARSER = "\\SQLParser.g4";
+    private final static String RESOUCE = new File("src/test/resources").getAbsolutePath();
+
+    @Test
+    void readParserTest() {
+        assertThrows(IllegalArgumentException.class,
+                () -> G4Reader.readParser(RESOUCE + PARSER.replace(".g4", ".mp4")));
+    }
+}
